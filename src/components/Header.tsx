abe5290c2c1f713269b5768a7d7d44a8f03f12ab
xiaoWangSec <124417044+xiaoWangSec@users.noreply.github.com>
2023-11-01 20:51:50 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,9 +33,9 @@ export default function Header() {
   const iconTitle = createMemo(() => splitEmoji(store.sessionSettings.title))
   return (
     <>
-      <header class="px-4 py-2 sticky top-0 z-99 flex justify-between items-center">
+{/*       <header class="px-4 py-2 sticky top-0 z-99 flex justify-between items-center">
         <ThemeToggle />
-      </header>
+      </header> */}
     </>
   )
 }
