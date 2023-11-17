81bf4b1d33939ee81fb07105b80800b977aad74d
xiaoWangSec <124417044+xiaoWangSec@users.noreply.github.com>
2023-11-17 12:43:27 +0800

    Update cron.ts

--- src/routes/api/cron.ts
+++ src/routes/api/cron.ts
@@ -34,7 +34,7 @@ export const config = {
     "syd1"
   ]
 }
-
+export const preferredRegion = ['arn1', 'bom1', 'cdg1', 'cle1', 'cpt1', 'dub1', 'fra1', 'gru1', 'hnd1', 'iad1', 'icn1', 'kix1', 'lhr1', 'pdx1', 'sfo1', 'sin1', 'syd1'];
 export async function GET() {
   try {
     const keys = Array.from(new Set(splitKeys(localKey)))
